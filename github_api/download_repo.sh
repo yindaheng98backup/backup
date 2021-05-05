aa765e6dc7ffaace6fd9400267a9c5aa77a946a1
yindaheng98 <yindaheng98@163.com>
2021-05-05 14:33:17 +0800

    仓库下载不成功就重试直到成功

--- github_api/download_repo.sh
+++ github_api/download_repo.sh
@@ -9,7 +9,10 @@ LOCAL=$4     #仓库目录
 URL="https://$TOKEN@github.com/$USER/$REPO_NAME.git"
 PRE=$(pwd)
 rm -rf $LOCAL
-git clone --mirror $URL $LOCAL/.git #下载仓库中的所有branch
+for ((SUCCESS = 1; SUCCESS != 0; )); do
+    git clone --mirror $URL $LOCAL/.git #下载仓库中的所有branch
+    SUCCESS=$?
+done # 也就下载仓库这里可能出乱子了，循环一下直到成功
 cd $LOCAL
 git config --bool core.bare false
 cd $PRE
